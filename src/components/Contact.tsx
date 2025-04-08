6607ef7ee6e127298be11b436eee863331bca5f3
Andy <andy_garlop4@hotmail.com>
2025-04-08 16:25:42 -0600

    Centrar titulo de contacto

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,10 +8,10 @@ export default function Contact() {
           <img className="w-[100%]" src="https://images.pexels.com/photos/6660071/pexels-photo-6660071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="imagen-contacto" />
         </div>
 
-        <div className="px-3 mt-5">
-          <h2 className=" pl-20 text-3xl text-center font-semibold">Datos De Contacto</h2>
+        <div className="mt-5">
+          <h2 className="text-3xl text-center font-semibold">Datos De Contacto</h2>
 
-          <div className="h-[20rem] flex flex-col justify-around items-start font-bold bg-amber-200/60 rounded-2xl p-5 my-5">
+          <div className="mx-3 h-[20rem] flex flex-col justify-around items-start font-bold bg-amber-200/50 rounded-2xl p-5 my-5">
             <p className="flex flex-col justify-center items-center w-full">Correo Electronico: <span className="font-normal text-center">nutritrack@correo.com</span></p>
             <p className="flex flex-col justify-center items-center w-full">Telefono: <span className="font-normal text-center">+52 33-12-34-56</span></p>
             <p className="flex flex-col justify-center items-center w-full">Domicilio: <span className="font-normal text-center">Av. Lazaro Cardenas, Lomas de los Arboles #4487</span></p>
